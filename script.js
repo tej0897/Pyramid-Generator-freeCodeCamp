a28fe30676034f0b4b8a6ead59ee99ffc563adac
Tejas Kumar S <tej0897@gmail.com>
2024-04-23 00:17:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 
-
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
 }
 
 let result = ""
